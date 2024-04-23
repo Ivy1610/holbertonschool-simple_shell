c1e21a9c9e6610cb7395e48482292e59ac013eff
fatmaGma <8891@holbertonstudents.com>
2024-04-23 07:34:01 +0000

    update for the main ( the /bin/ls work)

--- main.c
+++ main.c
@@ -1,46 +1,72 @@
-#include "shell.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
-
-/**
- * main - function main
- * Return: 0 if success
- */
+#define TOKEN_DELIM " \t\n"
 
 int main(void)
 {
-	char buffer[BUFFER_SIZE];
-	char bytes_read;
+	char *buffer;
+	size_t bufsize = BUFFER_SIZE;
+	ssize_t characters;
+	char *token;
+	char *args[BUFFER_SIZE];
+	pid_t pid;
+	int status;
+	int i;
 
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("malloc error");
+		exit(EXIT_FAILURE);
+	}
 	while (1)
 	{
-		write(STDOUT_FILENO, "CisNotFun$ ", 2);
-		bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-
-		if (bytes_read == -1)
+		printf("#cisfun$ ");
+		characters = getline(&buffer, &bufsize, stdin);
+		if (characters == -1)
 		{
-			perror("read");
-			exit(EXIT_FAILURE);
-		}
-		for (int i = 0; i < bytes_read; i++)
-		{
-			if (buffer[i] == '\n')
+			if (feof(stdin))
 			{
-				buffer[i] = '\0';
+				printf("\n");
 				break;
 			}
+			else
+			{
+				perror("getline error");
+				exit(EXIT_FAILURE);
+			}
 		}
-
-		int status = system(buffer);
-
-		if (status == -1)
+		buffer[strcspn(buffer, "\n")] = 0;
+		i = 0;
+		token = strtok(buffer, TOKEN_DELIM);
+		while (token != NULL)
 		{
-			perror("system");
+			args[i++] = token;
+			token = strtok(NULL, TOKEN_DELIM);
+		}
+		args[i] = NULL;
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork error");
 			exit(EXIT_FAILURE);
 		}
+		else if (pid == 0)
+		{
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror(args[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+			waitpid(pid, &status, 0);
 	}
-	return (0);
+	free(buffer);
+	return EXIT_SUCCESS;
 }
